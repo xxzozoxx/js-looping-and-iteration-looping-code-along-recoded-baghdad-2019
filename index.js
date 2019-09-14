1e33ee68c0c4b2d7d7b72a4d9d062791cb74c4da
Zahraa  Jalaa <zahraa.jalaa99@gmail.com>
2019-09-14 23:07:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+let msg;
+function writeCards(arr,event){
+  for(let i=0;i<arr.length;i++){
+   msg = console.log(`Thank you ${arr[i]} for the wonderful ${event} gift!`);
+  }
+   return msg;
+}
+writeCards(["Ada", "Brendan", "Ali"], "birthday");
+
+function countdown (num){
+  while (num >= 0){
+   console.log(num --);
+  }
+ 
+}
+countdown(10);
