197163b1e97113a5b0f6abda29ed4733f2f95b74
Zahraa  Jalaa <zahraa.jalaa99@gmail.com>
2019-09-14 23:12:38 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(arr,event){
+  let msg=[];
+  for(let i=0;i<arr.length;i++){
+    msg[i]=(`Thank you, ${arr[i]}, for the wonderful ${event} gift!`);
+  }
+   return msg;
+}
+writeCards(["Ada", "Brendan", "Ali"], "birthday");
+
+function countdown (num){
+  while (num >= 0){
+   console.log(num --);
+  }
+ 
+}
+countdown(10);
